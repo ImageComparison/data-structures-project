3161f921ad43e0be00cac133081c9f1754d2eb11
okikio <okikio.dev@gmail.com>
2022-04-13 02:55:09 -0400

    feat: finish barcode generation + angle projections

--- UWP_APP/QueryImage.cs
+++ UWP_APP/QueryImage.cs
@@ -3,31 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using NumSharp;
+using NumpyDotNet;
 
-namespace UWP_APP
+namespace ImageComparison
 {
     internal class QueryImage
     {
-        // Compare 2 bit strings, find how many bits are the same between them
-        //public int HammingDistance(String a, String b)
-        //{
-        //    var length = a.Length;
-        //    if (length != b.Length)
-        //        return -1;
 
-        //    List<char> chars1 = a.ToList();
-        //    List<char> chars2 = b.ToList();
+        public QueryImage() { }
 
-        //    var d = 0;
-        //    foreach ((char c1, char c2) in chars1.Zip(chars2, (a, b) => (a, b)))
-        //    {
-        //        if (c1 != c2) d++;
-        //    }
-        //    return 1 - (d / length);
-        //}
+        /**
+         * Converts a 2D list into a 2D Multidimentional Array (not a Jagged Array, yes there is a difference)
+         * > Note, not used. We switched to multidimentional arrays be defaults, so no more 2D Lists
+         * 
+         * @deprecated
+         */
+        private static T[,] ListToArray2D<T>(List<List<T>> input) 
+        {
+            var width = input[0].Count();
+            var height = input.Count();
+            T[,] arrays = new T[height, width];
 
-        // Compare 2 bit strings, find how many bits are the same between them
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                    arrays[i, j] = input[i][j];
+
+            return arrays;
+        }
+        /** 
+         * Compare 2 bit strings, find how many bits are the same between them. 
+         * The bit distances are range a between 0 and 1, they represent a percentage of the distance.
+         * 
+         * `-1`, represents an error in HammingDistance 
+         */
         public static float HammingDistance(String a, String b) { // O(n)
             var length = a.Length;
             if (length != b.Length)
@@ -39,16 +47,20 @@ namespace UWP_APP
                     distance++;
             }
 
-            return distance/(float)length;
+            return distance / (float)length;
         }
 
-        private static List<int> Project_0DEG(List<List<byte>> data) // O(n^2)
+        /**
+         * Create a horizontal projection of 2D Lists
+         */
+        private static List<int> Project_0DEG(byte[,] data) // O(n^2)
         {
-            List<int> result = new List<int>();
-            foreach (var row in Enumerable.Range(0, 28)) { // O(n)
+            var size = data.Length;
+            var result = new List<int>();
+            foreach (var row in Enumerable.Range(0, size)) { // O(n)
                 int rowSumData = 0;
-                foreach (var cell in Enumerable.Range(0, 28)) // O(n)
-                    rowSumData += data[row][cell];
+                foreach (var cell in Enumerable.Range(0, size)) // O(n)
+                    rowSumData += data[row, cell];
 
                 result.Add(rowSumData);
             }
@@ -56,14 +68,47 @@ namespace UWP_APP
             return result;
         }
 
-        private static List<int> Project_45DEG(List<List<byte>> data) // O(n^2)
+        /**
+         * Create a 45deg diagonal projection of 2D Lists
+         */
+        private static List<int> Project_45DEG(byte[,] arrays) // O(n^2)
         {
-            List<int> result = new List<int>();
-            foreach (var col in Enumerable.Range(0, 28))
+            var diags = new List<int>();
+
+            var data = np.array(arrays);
+
+            int start = (int)(-data.shape[0] + 1);
+            int end = (int)(data.shape[1]);
+
+            int count = (int)(data.shape[0] - 1) + end;
+
+            // Creates a range that starts at `start` but goes for `count` iterations
+            // The range is more like `[start...end] = [start...(start + count)]`
+            foreach (var i in Enumerable.Range(start, count))
+            {
+                var list = data
+                    .diagonal(i)
+                    .ToList<int>();
+
+                var sum = list.Sum();
+                diags.Add(sum);
+            }
+
+            return diags;
+        }
+
+        /**
+         * Create a vertical projection of 2D Lists
+         */
+        private static List<int> Project_90DEG(byte[,] data) // O(n^2)
+        {
+            var size = data.Length;
+            var result = new List<int>();
+            foreach (var col in Enumerable.Range(0, size))
             {
                 int colSumData = 0;
-                foreach (var cell in Enumerable.Range(0, 28))
-                    colSumData += data[cell][col];
+                foreach (var cell in Enumerable.Range(0, size))
+                    colSumData += data[cell,col];
 
                 result.Add(colSumData);
             }
@@ -71,114 +116,66 @@ namespace UWP_APP
             return result;
         }
 
-        private static List<int> Project_90DEG(List<List<byte>> data) // O(n^2)
+        /**
+         * Create a 135deg diagonal projection of 2D Lists
+         */
+        private static List<int> Project_135DEG(byte[,] arrays) // O(n^2)
         {
-            List<int> result = new List<int>();
-            foreach (var col in Enumerable.Range(0, 28))
-            {
-                int colSumData = 0;
-                foreach (var cell in Enumerable.Range(0, 28))
-                    colSumData += data[cell][col];
+            var diags = new List<int>();
 
-                result.Add(colSumData);
+            var data = np.array(arrays);
+
+            int start = (int)(-data.shape[0] + 1);
+            int end = (int)(data.shape[1]);
+
+            int count = (int)(data.shape[0] - 1) + end;
+
+            // Creates a range that starts at `start` but goes for `count` iterations
+            // The range is more like `[start...end] = [start...(start + count)]`
+            foreach (var i in Enumerable.Range(start, count))
+            {
+                // Flip the array left to right, so as to get the opposite diagonal
+                var list = np.fliplr(data)
+                    .diagonal(i)
+                    .ToList<int>();
+
+                var sum = list.Sum();
+                diags.Add(sum);
             }
 
-            return result;
+            return diags;
         }
 
-        private static List<int> Project_135DEG(List<List<byte>> data) // O(n^2)
-        {
-            // var rot_data = data
-                // .SelectMany(inner => inner.Select((item, index) => new { item, index }))
-                // .GroupBy(i => i.index, i => i.item)
-                // .Select(g => g.ToList())
-                // .ToList();
-            
-            // ROT_DATA IS INCORRECT, IT WILL YEILD THE SAME RESULTS AS 45 DEG
-            // BECAUSE THE MATRIX IS BEING ROTATED 180 DEGREES AND NOT 90 DEGREES
-            
-            
-            
-            List<int> result = new List<int>();
-            foreach (var col in Enumerable.Range(0, 28))
-            {
-                int colSumData = 0;
-                foreach (var cell in Enumerable.Range(0, 28))
-                    // colSumData += rot_data[cell][col]; // uncomment when working
-                    colSumData += data[cell][col]; // delete when working
-
-
-                result.Add(colSumData);
-            }
-
-            return result;
-        }
-        
+        /**
+         * Generates the barcode for an image if given its data as a list of bytes, and a width & height
+         */
         public static List<int> Generate_Barcode(List<byte> raw, int width, int height) // O(n^2)
         {
-            List<List<byte>> formatted_data = new List<List<byte>>();
-            
-            for (int i = 0; i < raw.Count; i+=width*4) //for each row // O(n)
-            {
-                List<byte> temp = new List<byte>();
-                // byte[] temp = {}; // ** replaced with line above **
-                for (int j = i; j < i + width*4; j+=4) //for each column in current row // O(n)
-                {
-                    byte new_pixel = (byte)(raw[j] * 0.11 + raw[j + 1] * 0.59 + raw[j + 2] * 0.3); //make pixel grayscale
-                    temp.Add(new_pixel); //add pixel to row
-                    // temp.Append(new_pixel); // ** replaced with line above **
-                    
-                    // List<int> result = new List<int>();
-                    // Console.WriteLine(result.Count);
-                    // result.Add(3);
-                    // Console.WriteLine(result.Count);
+            byte[,] data = { };
 
-                    //NEED TO USE VECTORS NOT ARRAYS
+            // For each row
+            for (int i = 0; i < raw.Count; i += width * 4) // O(n)
+            {
+                // For each column in current row 
+                for (int j = i; j < i + width * 4; j += 4) // O(n)
+                {
+                    // Make pixel grayscale
+                    byte pixel = (byte)(raw[j] * 0.11 + raw[j + 1] * 0.59 + raw[j + 2] * 0.3);
+                    data[i, j] = pixel; // Add pixel
                 }
-                formatted_data.Add(temp); //add row
             }
 
-            //getproj0
-            List<int> barcode0 = Project_0DEG(formatted_data);
-            //getproj45
-            List<int> barcode1 = Project_45DEG(formatted_data);
-            //getproj90
-            List<int> barcode2 = Project_90DEG(formatted_data);
-            //getproj135
-            List<int> barcode3 = new List<int>();//Project_135DEG(formatted_data);
+            // Different projection angles
+            var barcode0 = Project_0DEG(data);
+            var barcode1 = Project_45DEG(data);
+            var barcode2 = Project_90DEG(data);
+            var barcode3 = Project_135DEG(data);
 
-            return (barcode0.Concat(barcode1.Concat(barcode2.Concat(barcode3).ToList()).ToList()).ToList());
-        }
-
-        QueryImage() {
-
-
-
-//def project_45deg(data = []):
-//    diags = [data[::1,:].diagonal(i) for i in range(-data.shape[0]+1,data.shape[1])]
-//    counter = 0
-//    while counter < len(diags):
-//        diags[counter] = list(diags[counter])
-//        counter += 1
-//    counter = 0
-//    while counter < len(diags):
-//        if len(diags[counter]) == 1:
-//            diags.pop(counter)
-//        counter += 1
-//    return diags
-//def project_135deg(data = []):
-//    diags = [data[::-1,:].diagonal(i) for i in range(-data.shape[0]+1,data.shape[1])]
-//    counter = 0
-//    while counter < len(diags):
-//        diags[counter] = list(diags[counter])
-//        counter += 1
-//    counter = 0
-//    while counter < len(diags):
-//        if len(diags[counter]) == 1:
-//            diags.pop(counter)
-//        counter += 1
-//    return diags
-
+            return barcode0
+                .Concat(barcode1)
+                .Concat(barcode2)
+                .Concat(barcode3)
+                .ToList();
         }
     }
 }
